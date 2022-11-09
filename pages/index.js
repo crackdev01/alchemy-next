fe3ef77b39610cc72236d21a0da6b54fe17a5dc7
hiroshi <blackbox0823@gmail.com>
2022-11-09 11:53:55 +0900

    recover dashboard

--- pages/index.js
+++ pages/index.js
@@ -18,10 +18,12 @@ import {
 // pick a theme of your choice
 import original from 'react95/dist/themes/original';
 // original Windows95 font (optionally)
-import { useAccount, useConnect, useEnsName, useNetwork } from 'wagmi'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import ERC721 from '../abis/ERC721.json'
-import { Wrapper, GlobalStyles } from '../styles';
+import { useAccount, useConnect, useEnsName, useNetwork } from 'wagmi';
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+import ERC721 from '../abis/ERC721.json';
+import { Wrapper, GlobalStyles } from '../Styles/Styles';
+
 import InfoWindow from '../windows/Info';
 import IexploreWindow from '../windows/Iexplore';
 import StakeWindow from '../windows/staking/Stake';
@@ -34,21 +36,26 @@ import { CreateOffer } from '../windows/offermgr/CreateOffer';
 import { Swap } from '../windows/marketplace/Swap';
 import { MyListings } from '../windows/listingmgr/MyPools';
 import { XSushiStaking } from '../windows/staking/xSushiStaking';
+import { Rnd } from 'react-rnd';
+import {
+  useWindowSize,
+} from '@react-hook/window-size';
 
-export default function Home() {
+
+export default function Default() {
 
   const [open, setOpen] = React.useState(false);
   const [welcomeWindow, setWelcomeWindow] = React.useState(true);
   const [iexploreWindow, setIexploreWindow] = React.useState(false);
   const [initialized, setInitialized] = React.useState(false);
 
-  const [iframesrc, _setIframesrc] = React.useState("")
+  const [iframesrc, _setIframesrc] = React.useState("");
 
 
-  const [isEligible, setIsEligible] = React.useState(false || process.env.NODE_ENV === 'development')
-  const windowStack = useSelector((state) => state.openWindow)
-  const dispatch = useDispatch()
-  const setWindowStack = (a) => dispatch(openWindow(a))
+  const [isEligible, setIsEligible] = React.useState(false || process.env.NODE_ENV === 'development');
+  const windowStack = useSelector((state) => state.openWindow);
+  const dispatch = useDispatch();
+  const setWindowStack = (a) => dispatch(openWindow(a));
   const windows = {
     n00d: <InfoWindow key={'n00d'} isEligible={isEligible} setIframesrc={setIframesrc} setWindowStack={setWindowStack}></InfoWindow>,
     iexplore: <IexploreWindow iframesrc={iframesrc}></IexploreWindow>,
@@ -60,40 +67,57 @@ export default function Home() {
     createoffer: <CreateOffer></CreateOffer>,
     sweep: <Swap></Swap>,
     mypools: <MyListings></MyListings>,
-  }
-  const spawnStraySheep = () => {
-    const esheep = new window.eSheep()
-    esheep.Start()
-  }
-  React.useEffect(() => {
+  };
+  const [width, height] = useWindowSize();
 
+  const spawnStraySheep = () => {
+    const esheep = new window.eSheep();
+    esheep.Start();
+  };
+  React.useEffect(() => {
     if (window.eSheep) {
       if (!initialized) {
-        const esheep = new window.eSheep()
-        esheep.Start()
-        setWindowStack({ action: 'push', window: 'n00d' })
-        setInitialized(true)
+        const esheep = new window.eSheep();
+        esheep.Start();
+        setWindowStack({ action: 'push', window: 'n00d' });
+        setInitialized(true);
       }
     }
-  })
+  });
+
+  React.useEffect(() => {
+    if (Object.entries(windowPositions).length > 0) {
+      Object.entries(windowPositions).forEach(([key, value]) => {
+        if (value) {
+          if (width < value.x) {
+            setWindowPositions({ [key]: { ...value, x: width - 200 } });
+          }
+
+          if (height < value.y) {
+            setWindowPositions({ [key]: { ...value, y: height - 200 } });
+          }
+        }
+      });
+    }
+  }, [width, height]);
+
   function setIframesrc(x) {
-    if (windowStack.indexOf('iexplore') === -1) setWindowStack({ action: 'push', window: 'iexplore' })
-    _setIframesrc(x)
+    if (windowStack.indexOf('iexplore') === -1) setWindowStack({ action: 'push', window: 'iexplore' });
+    _setIframesrc(x);
   }
-  const { chain } = useNetwork()
-  if (chain?.id && chain?.id != 1 && chain?.id != 5) alert(`Network ${chain?.id} not supported`)
-  const { address, isConnected } = useAccount()
-  const { data: ensName } = useEnsName({ address })
+  const { chain } = useNetwork();
+  if (chain?.id && chain?.id != 1 && chain?.id != 5) alert(`Network ${chain?.id} not supported`);
+  const { address, isConnected } = useAccount();
+  const { data: ensName } = useEnsName({ address });
   const { connect } = useConnect({
     connector: new InjectedConnector(),
-  })
-  const [windowPositions, setWindowPositions] = React.useReducer((res, action) => {
-    res[action.window] = action.data
-    return res
-  }, {})
+  });
+  const [windowPositions, setWindowPositions] = React.useState({});
+  const [windowSizes, setWindowSizes] = React.useState({});
+  console.log(windowSizes)
   return (
     <>
-      <Head>
+    <Head>
         <meta charset="utf-8" />
         <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -108,125 +132,168 @@ export default function Home() {
         <title>n00dleSwap</title>
       </Head>
       <Wrapper>
-        <GlobalStyles></GlobalStyles>
-        <ThemeProvider theme={original} >
-          {
-            windowStack.map((window, i) => <Draggable
-              onStop={(e, data) => {
-                setWindowPositions({ window, data })
-              }}
-              defaultPosition={windowPositions[window]}
-              onMouseDown={
-                () => {
-                  setWindowStack({ action: 'focus', window })
-                  setIexploreWindow(!iexploreWindow)
-                }} key={window + i} handle=".window-header" ><Window
-                  style={{ position: 'absolute' }}
-                  className='window'>
-                <WindowHeader active={i === windowStack.length - 1} className='window-header'>
-                  <span>{window}.exe</span>
-                  <Button onClick={(event) => {
-                    setWindowStack({ action: 'del', window })
-                    setWelcomeWindow(!welcomeWindow)
-                    event.stopPropagation()
-                  }}>
-                    <span className='close-icon' />
-                  </Button>
-                </WindowHeader>{windows[window]}
-              </Window>
-            </Draggable>)
+      <GlobalStyles></GlobalStyles>
+      <ThemeProvider theme={original}>
+        {
+          windowStack.map((window, i) => {
+            return (
+              <Rnd
+                key={window + i}
+                onDragStop={(e, data) => {
+                  setWindowPositions(pos => {
+                    return {  ...pos, [window]: data }
+                  });
+                }}
+                onResizeStop={(e, data, ref) => {
+                  setWindowSizes(sizes => {
+                    return {  ...sizes, [window]: ref.style }
+                  });
+                  //e.stopImmediatePropagation()
+                }}
+                onClick={
+                  () => {
+                    setWindowStack({ action: 'focus', window: window });
+                    setIexploreWindow(!iexploreWindow);
+                  }}
+                default={{
+                  x: (width / 2 - 200) + (i * 40),
+                  y: 50 + (i * 40),
+                  width: 1000,
+                }}
+                position={windowPositions[window]}
+                size={windowSizes[window]}
+                minWidth={300}
+                dragHandleClassName="window-header"
+                enableResizing={{
+                  bottom: true,
+                  bottomLeft: false,
+                  bottomRight: true,
+                  left: false,
+                  right: true,
+                  top: false,
+                  topLeft: false,
+                  topRight: false
+                }}
+                maxWidth={'100vw'}
+              >
+                <Window  style={{ width: '100%', height: '100%' }} className="window">
+
+                  <WindowHeader active={i === windowStack.length - 1} className='window-header'>
+                    <span>{window}.exe</span>
+                    <div style={{ display: 'flex', justifyContent: 'space-evenly', gap: '0.25rem' }}>
+                      {/* <Button onClick={(event) => {
+                        setWindowStack({ action: 'max', window: win });
+                      }}>
+                      </Button> */}
+                      <Button onClick={(event) => {
+                        setWindowStack({ action: 'del', window: window });
+                        setWelcomeWindow(!welcomeWindow);
+                        event.stopPropagation();
+                      }}>
+                        <span className='close-icon' />
+                      </Button>
+                    </div>
+                  </WindowHeader><div style={{overflowY: 'scroll', overflowX: 'hidden', height: 'calc(100% - 2.5em)'}}>{windows[window]}</div>
+                </Window>
+              </Rnd>
+            );
           }
 
-          <AppBar>
-            <Toolbar style={{ justifyContent: 'space-between' }}>
-              <div style={{ position: 'relative', display: 'inline-block' }}>
-                <Button
-                  onClick={() => setOpen(!open)}
-                  active={open}
-                  style={{ fontWeight: 'bold' }}
+
+          )
+        }
+
+        <AppBar>
+          <Toolbar style={{ justifyContent: 'space-between' }}>
+            <div style={{ position: 'relative', display: 'inline-block' }}>
+              <Button
+                onClick={() => setOpen(!open)}
+                active={open}
+                style={{ fontWeight: 'bold' }}
+              >
+                <Image src="/assets/noodlogo.png" alt="Vercel Logo" width={20} height={20} marginRight={4} />
+                Start
+              </Button>
+              {open && (
+                <List
+                  style={{
+                    position: 'absolute',
+                    left: '0',
+                    top: '100%',
+                  }}
+                  onClick={() => setOpen(false)}
                 >
-                  <Image src="/assets/noodlogo.png" alt="Vercel Logo" width={32} height={20} marginRight={4} />
-                  Start
-                </Button>
-                {open && (
-                  <List
-                    style={{
-                      position: 'absolute',
-                      left: '0',
-                      top: '100%',
-                    }}
-                    onClick={() => setOpen(false)}
-                  >
-                    <ListItem disabled={address} onClick={connect}>
-                      <span role='img' aria-label='🔗'>
-                        🔗
-                      </span>
-                      {address ? 'Connected' : 'Connect Wallet'}
-                    </ListItem>
-                    <Divider></Divider>
-                    <ListItem  onClick={() => setWindowStack({ action: 'push', window: 'x' })}>
-                      <span role='img' aria-label='👨‍🍳' >
-                        👨‍🍳
-                      </span>
-                      &nbsp;Prepare Meal (Earn fees)
-                    </ListItem>
-                    <ListItem  onClick={() => setWindowStack({ action: 'push', window: 'stake' })}>
-                      <span role='img' aria-label='🍴' >
-                        🍴
-                      </span>
-                      Dining Table (Staking)
-                    </ListItem>
-                    <Divider />
-                    <ListItem  onClick={() => setWindowStack({ action: 'push', window: 'nftselector' })}>
-                      <span role='img' aria-label='🤑' >
-                        🤑
-                      </span>
-                      List your NFT
-                    </ListItem>
-                    <ListItem  onClick={() => setWindowStack({ action: 'push', window: 'createoffer' })}>
-                      <span role='img' aria-label='💱' >
-                        💱
-                      </span>
-                      Create offer for NFT
-                    </ListItem>
-                    <ListItem  onClick={() => setWindowStack({ action: 'push', window: 'mypools' })}>
-                      <span role='img' aria-label='🏊' >
-                        🏊
-                      </span>
-                      My Pools
-                    </ListItem>
-                    <Divider></Divider>
-                    <ListItem onClick={() => setWindowStack({ action: 'push', window: 'sweep' })}>
-                      <span role='img' aria-label='🔀'>
-                        🔀
-                      </span>
-                      Sweep NFTs
-                    </ListItem>
-                    <Divider />
+                  <ListItem disabled={address} onClick={connect}>
+                    <span role='img' aria-label='🔗'>
+                      🔗
+                    </span>
+                    {address ? 'Connected' : 'Connect Wallet'}
+                  </ListItem>
+                  {/* <Divider></Divider>
+                  <ListItem disabled={!address} onClick={() => setWindowStack({ action: 'push', window: 'x' })}>
+                    <span role='img' aria-label='👨‍🍳' >
+                      👨‍🍳
+                    </span>
+                    &nbsp;Prepare Meal (Earn fees)
+                  </ListItem>
+                  <ListItem disabled={!address} onClick={() => setWindowStack({ action: 'push', window: 'stake' })}>
+                    <span role='img' aria-label='🍴' >
+                      🍴
+                    </span>
+                    Dining Table (Staking)
+                  </ListItem> */}
+                  <Divider />
+                  <ListItem disabled={!address} onClick={() => setWindowStack({ action: 'push', window: 'nftselector' })}>
+                    <span role='img' aria-label='🤑' >
+                      🤑
+                    </span>
+                    List your NFT
+                  </ListItem>
+                  <ListItem disabled={!address} onClick={() => setWindowStack({ action: 'push', window: 'createoffer' })}>
+                    <span role='img' aria-label='💱' >
+                      💱
+                    </span>
+                    Create offer for NFT
+                  </ListItem>
+                  <ListItem disabled={!address} onClick={() => setWindowStack({ action: 'push', window: 'mypools' })}>
+                    <span role='img' aria-label='🏊' >
+                      🏊
+                    </span>
+                    My Pools
+                  </ListItem>
+                  <Divider></Divider>
+                  <ListItem disabled={!address} onClick={() => setWindowStack({ action: 'push', window: 'sweep' })}>
+                    <span role='img' aria-label='🔀'>
+                      🔀
+                    </span>
+                    Sweep NFTs
+                  </ListItem>
+                  <Divider />
 
-                    <ListItem onClick={() => {
-                      setWindowStack({ action: 'push', window: 'n00d' })
-                      setWelcomeWindow(true)
-                    }}>
-                      <span role='img' aria-label='👨‍💻'>
-                        👨‍💻
-                      </span>
-                      Info
-                    </ListItem>
-                    <ListItem onClick={() => {
-                      spawnStraySheep()
-                    }}>
-                      straysheep.exe
-                    </ListItem>
-                  </List>
-                )}
-              </div>
-            </Toolbar>
-          </AppBar>
+                  <ListItem onClick={() => {
+                    setWindowStack({ action: 'push', window: 'n00d' });
+                    setWelcomeWindow(true);
+                  }}>
+                    <span role='img' aria-label='👨‍💻'>
+                      👨‍💻
+                    </span>
+                    Info
+                  </ListItem>
+                  <ListItem onClick={() => {
+                    spawnStraySheep();
+                  }}>
+                    straysheep.exe
+                  </ListItem>
+                </List>
+              )}
+            </div>
+          </Toolbar>
+        </AppBar>
 
-        </ThemeProvider>
+      </ThemeProvider>
 
-      </Wrapper>
+    </Wrapper>
     </>
-  )
+
+  );
 }
