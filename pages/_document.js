7330605e82dee20a86f153ec4f74537cf435e93f
hiroshi <blackbox0823@gmail.com>
2022-11-09 11:56:09 +0900

    fix document

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@4.2.95/css/materialdesignicons.min.css" />
       </Head>
       <body>
         <Main />
